54ba880ef2b69e220125c200bc11cc8819841c38
Krystian Kowal <krystian@kowal.pro>
2019-01-06 21:17:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Pure JS App Boilerplate
+
+This is bolireplate for pure JS app.
+
+## Features
+
+- webpack
+- babel
+- webpack dev server
+- hot module replacement
+- css loader
+
+## Usage
+
+- clone repo
+- rename folder from `pure-js-app-boilerplate` to your app name
+- change name in `package.json`
+- delete git folder `rm -rf .git`
+- create new git repo `git init`
+- make changes and commit
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
